4308cff7411cd2756b176b8e4d4fc7df4eabb5ee
andrevale99 <andrefvale@gmail.com>
2023-01-15 16:06:50 -0300

    Codigo finalizado com os comentarios

--- Interrupts/Blink_Assembly/main.asm
+++ Interrupts/Blink_Assembly/main.asm
@@ -6,10 +6,14 @@
 .cseg
 .org 0x00
 
+;	Configura  o endereco absoluto do vetor de interrupcao
+;na SRAM
 .org 0x0016
 	jmp TIM1_COMPA
 
 Setup:
+	;	Inicializa e configura os vetores de interrupcao
+	;trecho retirado do datasheet
 	ldi r16, high(RAMEND)
 	out SPH, r16
 	ldi r16, low(RAMEND)
@@ -25,21 +29,24 @@ Setup:
 	sbi DDRB, DDB5
 	cbi PORTB, PB5
 
+	;	Escreve o prescale de 16e6/1024
+	;e o modo CTC do Timer 1
 	clr r16
 	sbr r16, (1<<WGM12) + (1<<CS12) + (1<<CS10)
-	sts TCCR1B, r16
+	sts TCCR1B, r16 
 
+	;Ativa a interrupcao por comparacao
 	clr r16
 	sbr r16, (1<<OCIE1A)
 	sts TIMSK1, r16
 
 	clr r16
 
-	ldi r16, 0x09
-	ldi r17, 0x3D
+	ldi r16, 0x09  
+	ldi r17, 0x3D 
 
-	sts OCR1AH, r17
-	sts OCR1AL, r16
+	sts OCR1AH, r17 ;Escreve o valor de 15625 
+	sts OCR1AL, r16 ;nos registradores MSB e LSB do comparador
 	
 	clr r16
 	clr r17
@@ -50,10 +57,10 @@ Setup:
 
 
 TIM1_COMPA:
-	ldi r16, 0x20
-	in r17, PINB
-	eor r17, r16
-	out PORTB, r17
+	ldi r16, 0x20 
+	in r17, PINB ;Ler a as portas dos PORTB
+	eor r17, r16 ;Realiza a troca do pino PB5
+	out PORTB, r17 ;Escreve no PORTB
 
 	clr r16
 	clr r17
