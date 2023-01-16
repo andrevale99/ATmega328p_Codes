86fcb0adaeb9a4cb2278180c2aa3f951c7d63e40
andrevale99 <andrefvale@gmail.com>
2023-01-16 15:02:15 -0300

    Codigo que utilizara as interrupcoes para transmitir caractere

---
+++ Interrupts/Tx_Interrupt/main.c
@@ -0,0 +1,60 @@
+/**
+ * @author Andre Menezes de Freitas Vale
+ * 
+ * @brief Codigo que utiliza da interrupcao externas PCINTXX
+ * para criar uma logica de semaforo simples 
+ * 
+*/
+
+#ifndef F_CPU
+    #define F_CPU 16000000UL
+#endif
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define SetBit(port, pin) (port |= (1<<pin))
+#define ClrBit(port, pin) (port &= ~(1<<pin))
+#define ToggleBit(port, pin) (port ^= (1<<pin))
+#define TestBit(port, pin) (port & (1<<pin))
+
+//===================================================
+//  VARIAVEIS
+//===================================================
+
+
+//===================================================
+//  PROTOTIPOS
+//===================================================
+void setup();
+
+//===================================================
+//  MAIN
+//===================================================
+int main()
+{
+    setup();
+    sei();
+
+    for(;;)
+    {
+
+    }
+
+    return 0;
+}
+
+//===================================================
+//  FUNCOES
+//===================================================
+/**
+ * @brief Funcao de setup
+*/
+void setup()
+{
+
+}
